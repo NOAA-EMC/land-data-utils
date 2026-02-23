6e9325324a4e6b4bddddce06b8f496a9e8c96730
Michael Barlage <michael.barlage@noaa.gov>
2026-02-23 17:19:23 +0000

    add general support for subsetting in weights

--- ufs-land-driver/weights/create_weights.sh
+++ ufs-land-driver/weights/create_weights.sh
@@ -26,13 +26,26 @@ module load stack-intel-oneapi-mpi/2021.13
 module load esmf/8.8.0
 module load ncl/6.6.2
 
-atm_res="C918"
+# set parameters for grid generation
+#
+# atm_res      : fv3 grid resolution
+# ocn_res      : ocean resolution, not used for AQM or ARC regional grids
+# grid_version : 20231027 - append directory date string
+#                AQM - AQM regional grid
+#                ARC - UFS-Arctic regional grid
+# fixfile_path : top level path for fix files
+# grid_extent  : total - use all grids (e.g., global or entire regional)
+#                subset - regional cutout, limits below
+# subset_name  : if subset, name for subset, e.g., conus
+
+atm_res="C96"
 ocn_res="mx100"
-grid_version="ARC"
+grid_version="20231027"
+grid_extent="subset"
+subset_name="conus"
 datm_source="ERA5"
 datm_source_file="/scratch4/NCEPDEV/land/data/ufs-land-driver/datm/ERA5/original/1980/ERA5_forcing_1980-01-01.nc"
 destination_scrip_path="/scratch4/NCEPDEV/land/data/ufs-land-driver/vector_inputs/"
-grid_extent="ARC"
 
 #################################################################################
 #  shouldn't need to modify anything below
@@ -49,31 +62,24 @@ echo "datm_source_file = $datm_source_file" >> regrid_parameter_assignment
 
 eval "time ncl create_datm_scrip.ncl"
 
-if [ $grid_version = "hr3" ]; then 
-  grid=$atm_res.$ocn_res"_hr3"
-elif [ $grid_version = "AQM" ]; then 
-  grid=$atm_res.$grid_version
-elif [ $grid_version = "ARC" ]; then 
-  grid=$atm_res.$grid_version
+if [[ $grid_version == "20231027" ]] ; then 
+  grid_string=$atm_res.$ocn_res
+  if [[ $grid_extent == "subset" ]]; then
+    grid_string=$grid_string.$subset_name
+  fi
+elif [[ $grid_version == "AQM" ]] || [[ $grid_version == "ARC" ]]; then 
+  grid_string=$atm_res.$grid_extent
 else
-  echo "ERROR: unknown grid_version $grid_version"
+  echo "ERROR: unknown combination"
+  echo "ERROR: grid_version = $grid_version"
+  echo "ERROR: grid_extent = $grid_extent"
+  echo "NOTE:  subset not currently supported for regional grids"
   exit 1
 fi
 
-if [ $grid_extent = "global" ]; then 
-  output_path=$atm_res.$ocn_res"/"
-elif [ $grid_extent = "AQM" ]; then 
-  output_path=$atm_res.$grid_extent"/"
-elif [ $grid_extent = "ARC" ]; then 
-  output_path=$atm_res.$grid_extent"/"
-elif [ $grid_extent = "conus" ]; then 
-  output_path=$atm_res.$ocn_res.$grid_extent"/"
-else
-  echo "ERROR: unknown grid_extent $grid_extent"
-  exit 2
-fi
+output_path=$grid_string"/"
 
-destination_scrip_file=$destination_scrip_path"/"$output_path"/ufs-land_"$grid"_SCRIP.nc"
+destination_scrip_file=$destination_scrip_path$output_path"ufs-land_"$grid_string"_SCRIP.nc"
 
 if [ -d $output_path ]; then 
   echo "BEWARE: output_path directory exists and overwriting is allowed"
@@ -84,7 +90,7 @@ fi
 # create weights file for bilinear interpolation
 
 interpolation_method="bilinear"
-weights_filename=$datm_source"-"$grid"_"$interpolation_method"_wts.nc"
+weights_filename=$datm_source"-"$grid_string"_"$interpolation_method"_wts.nc"
 echo "Creating weights file: "$weights_filename
 
 srun -n $SLURM_NTASKS time ESMF_RegridWeightGen --netcdf4 --ignore_degenerate \
@@ -95,7 +101,7 @@ srun -n $SLURM_NTASKS time ESMF_RegridWeightGen --netcdf4 --ignore_degenerate \
 # create weights file for neareststod interpolation
 
 interpolation_method="neareststod"
-weights_filename=$datm_source"-"$grid"_"$interpolation_method"_wts.nc"
+weights_filename=$datm_source"-"$grid_string"_"$interpolation_method"_wts.nc"
 echo "Creating weights file: "$weights_filename
 
 srun -n $SLURM_NTASKS time ESMF_RegridWeightGen --netcdf4 --ignore_degenerate \
@@ -103,3 +109,5 @@ srun -n $SLURM_NTASKS time ESMF_RegridWeightGen --netcdf4 --ignore_degenerate \
        --destination $destination_scrip_file \
        --weight $output_path$weights_filename --method $interpolation_method
 
+rm regrid_parameter_assignment
+rm PET*
